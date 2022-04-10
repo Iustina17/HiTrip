eeb87c4cdc0e712a47231dea704e9ec48431e466
IustinaRadulescu <iustina.radulescu@stud.acs.upb.ro>
2022-04-11 01:40:17 +0300

    Create Trip stage3 - complete info

--- app/src/main/java/com/tripshare/hitrip/CreateTrip1.java
+++ app/src/main/java/com/tripshare/hitrip/CreateTrip1.java
@@ -1,32 +1,33 @@
 package com.tripshare.hitrip;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.app.DatePickerDialog;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
-import com.tripshare.hitrip.Trips.MainActivity;
 
 import java.util.Calendar;
 
 public class CreateTrip1 extends AppCompatActivity implements View.OnClickListener {
-    EditText date_start;
+    static EditText date_start;
+    static EditText date_end;
     DatePickerDialog.OnDateSetListener setListener;
-    Spinner spinner;
-    ArrayAdapter<CharSequence> adapter;
+    Spinner spinner1, spinner2, spinner3;
+    LinearLayout layoutGradDif;
+
+    ArrayAdapter<CharSequence> adapter1,adapter2, adapter3;
     LinearLayout layoutList;
     Button buttonAdd;
 
@@ -36,32 +37,37 @@ public class CreateTrip1 extends AppCompatActivity implements View.OnClickListen
         setContentView(R.layout.activity_create_trip1);
 
         date_start = findViewById(R.id.date_start);
+        date_end = findViewById(R.id.date_end);
+
 
         layoutList = findViewById(R.id.layout_list);
         buttonAdd = findViewById(R.id.button_add_stops);
 
         buttonAdd.setOnClickListener(this);
 
-        Calendar calendar = Calendar.getInstance();
-        final int year = calendar.get(Calendar.YEAR);
-        final int month = calendar.get(Calendar.MONTH);
-        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+        layoutGradDif = findViewById(R.id.layout_grad_dificultate);
 
-        date_start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DatePickerDialog datePickerDialog = new DatePickerDialog(
-                        CreateTrip1.this, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int month, int day) {
-                        month = month+1;
-                        String date = day+"/"+month+"/"+year+"/";
-                        date_start.setText(date);
-                    }
-                }, year, month, day);
-                datePickerDialog.show();
-            }
-        });
+//        Calendar calendar = Calendar.getInstance();
+//        final int year = calendar.get(Calendar.YEAR);
+//        final int month = calendar.get(Calendar.MONTH);
+//        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+//
+//        date_start.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                DatePickerDialog datePickerDialog = new DatePickerDialog(
+//                        CreateTrip1.this, new DatePickerDialog.OnDateSetListener() {
+//                    @Override
+//                    public void onDateSet(DatePicker view, int year, int month, int day) {
+//                        month = month+1;
+//                        String date = day+"/"+month+"/"+year;
+//                        date_start.setText(date);
+//                    }
+//                }, year, month, day);
+//                datePickerDialog.getDatePicker().setMinDate(calendar.getTimeInMillis());
+//                datePickerDialog.show();
+//            }
+//        });
 
 //        setListener = new DatePickerDialog.OnDateSetListener() {
 //            @Override
@@ -72,12 +78,58 @@ public class CreateTrip1 extends AppCompatActivity implements View.OnClickListen
 //            }
 //        };
 
-        spinner = (Spinner) findViewById(R.id.spinner_tip_excursie);
-        adapter = ArrayAdapter.createFromResource(this, R.array.tip_excursie, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        date_start = (EditText) findViewById(R.id.date_start);
+        date_end = (EditText) findViewById(R.id.date_end);
+        date_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showTruitonDatePickerDialog(view);
+                date_start.setError(null);
+            }
+        });
+        date_end.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input = date_start.getText().toString();
+                if(input.equals("")){
+                    date_start.setError("Introduceţi data de început");
+                    Toast.makeText(CreateTrip1.this, "Introduceţi data de început", Toast.LENGTH_LONG).show();
+                    date_start.requestFocus();
+                    return;
+                }else{
+                    date_start.setError(null);
+                showToDatePickerDialog(view);
+                }
+            }
+        });
 
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        spinner1 = (Spinner) findViewById(R.id.spinner_tip_excursie);
+        adapter1 = ArrayAdapter.createFromResource(this, R.array.tip_excursie, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String text = spinner1.getSelectedItem().toString();
+                if(text.equals("Drumeţie")){
+                    layoutGradDif.setVisibility(View.VISIBLE);
+                }else{
+                    layoutGradDif.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        spinner2 = (Spinner) findViewById(R.id.grad_dificultate);
+        adapter2 = ArrayAdapter.createFromResource(this, R.array.grad_dificultate, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
             }
@@ -86,6 +138,95 @@ public class CreateTrip1 extends AppCompatActivity implements View.OnClickListen
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
+
+        spinner3 = (Spinner) findViewById(R.id.spinner_moneda);
+        adapter3 = ArrayAdapter.createFromResource(this, R.array.moneda, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adapter3);
+
+        spinner3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+
+//        spinner1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String text = spinner1.getSelectedItem().toString();
+//                if(text.equals("Drumeţie")){
+//                    layoutGradDif.setVisibility(View.VISIBLE);
+//                }else{
+//                    layoutGradDif.setVisibility(View.GONE);
+//                }
+//            }
+//        });
+    }
+
+    private void showTruitonDatePickerDialog(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    private void showToDatePickerDialog(View view) {
+        DialogFragment newFragment = new ToDatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public static class DatePickerFragment extends DialogFragment implements
+            DatePickerDialog.OnDateSetListener {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            DatePickerDialog datePickerDialog;
+            datePickerDialog = new DatePickerDialog(getActivity(), this, year,
+                    month, day);
+            datePickerDialog.getDatePicker().setMinDate(c.getTimeInMillis());
+            return datePickerDialog;
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            month = month + 1;
+            date_start.setText(day + "/" + month + "/" + year);
+        }
+
+    }
+
+    public static class ToDatePickerFragment extends DialogFragment implements
+            DatePickerDialog.OnDateSetListener {
+        // Calendar startDateCalendar=Calendar.getInstance();
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+
+            String getfromdate = date_start.getText().toString().trim();
+            String getfrom[] = getfromdate.split("/");
+            int year, month, day;
+            year = Integer.parseInt(getfrom[2]);
+            month = Integer.parseInt(getfrom[1]);
+            month = month - 1;
+            day = Integer.parseInt(getfrom[0]);
+            final Calendar c = Calendar.getInstance();
+            c.set(year, month, day);
+            DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), this, year, month, day);
+            datePickerDialog.getDatePicker().setMinDate(c.getTimeInMillis());
+            return datePickerDialog;
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            month = month + 1;
+            date_end.setText(day + "/" + month + "/" + year);
+        }
     }
 
     @Override
@@ -94,10 +235,11 @@ public class CreateTrip1 extends AppCompatActivity implements View.OnClickListen
     }
 
     private void addView() {
-        View stopView = getLayoutInflater().inflate(R.layout.row_add_stop,null,false);
-        EditText editText1 = (EditText)stopView.findViewById(R.id.locatie_item);
-        EditText editText2 = (EditText)stopView.findViewById(R.id.descriere_item);
-        ImageButton stopDelete = (ImageButton)stopView.findViewById(R.id.close_stop_button);
+        View stopView = getLayoutInflater().inflate(R.layout.row_add_stop, null, false);
+        EditText editText1 = (EditText) stopView.findViewById(R.id.locatie_item);
+        EditText editText2 = (EditText) stopView.findViewById(R.id.descriere_item);
+        EditText editText3 = (EditText) stopView.findViewById(R.id.descriere_transport);
+        ImageButton stopDelete = (ImageButton) stopView.findViewById(R.id.close_stop_button);
 
         stopDelete.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -109,7 +251,7 @@ public class CreateTrip1 extends AppCompatActivity implements View.OnClickListen
         layoutList.addView(stopView);
     }
 
-    private void removeView(View view){
+    private void removeView(View view) {
         layoutList.removeView(view);
     }
 }
