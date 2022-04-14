4fee8e9e3f33d5af8c7d24e6d99eaa0a0874e088
IustinaRadulescu <iustina.radulescu@stud.acs.upb.ro>
2022-04-14 22:09:19 +0300

    Profile - stage1- profile,my_profile,edit_profile,edit_personal_profile - without linking pages, buttons functionality, and details

---
+++ app/src/main/java/com/tripshare/hitrip/EditPersonalInfoActivity.java
@@ -0,0 +1,14 @@
+package com.tripshare.hitrip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditPersonalInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_personal_info);
+    }
+}
